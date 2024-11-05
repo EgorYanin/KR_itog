d14e3413b1e9c0e77a4d87f76be57105b684309a
EgorYanin <egor.yanin@list.ru>
2024-11-05 23:40:36 +0300

    Add files via upload

--- js/zadacha.js
+++ js/zadacha.js
@@ -1,1 +1,17 @@
+function checkIfCanCut() {
+    const D = parseFloat(document.getElementById("diameter").value);
+    const A = parseFloat(document.getElementById("width").value);
 
+
+    if (D < 0 || A < 0) {
+        document.getElementById("result").innerText = "Ошибка: отрицательные значения";
+        return;
+    }
+
+
+    if (A * Math.sqrt(2) <= D) {
+        document.getElementById("result").innerText = "Да, можно выпилить квадратный брус шириной " + A + ".";
+    } else {
+        document.getElementById("result").innerText = "Нет, невозможно выпилить квадратный брус шириной " + A + ".";
+    }
+}
